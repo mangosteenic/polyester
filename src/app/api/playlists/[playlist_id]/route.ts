a7f3fecb7f93c101b3318d2132d82c9ba074122d
Hannah Hong Nguyen <147587888+mangosteenic@users.noreply.github.com>
2025-08-19 13:55:00 -0500

    playlist id route

--- src/app/api/playlists/[playlist_id]/route.ts
+++ src/app/api/playlists/[playlist_id]/route.ts
@@ -4,15 +4,23 @@ export async function GET(
   request: Request,
   { params }: { params: Promise<{ playlist_id: string }> }
 ) {
-  const { playlist_id } = await params
+  const { playlist_id } =  await params
 
-  const result = await getPlaylist(playlist_id);
-
-  return new Response(JSON.stringify(result), {
+  try {
+    const result = await getPlaylist(playlist_id);
+    if (!result) {
+      return new Response("Playlist not found", { status: 404 });
+    }
+    
+    return new Response(JSON.stringify(result), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
-    },
-  });
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching playlist:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
 
